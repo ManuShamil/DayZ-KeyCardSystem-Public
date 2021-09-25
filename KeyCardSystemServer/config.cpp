25c59c4eb83207b5dc29f5b4e81d60fa6dfecf75
Manu Shamil <42031273+ManuShamil@users.noreply.github.com>
2021-09-25 15:14:00 +0530

    update

--- KeyCardSystemServer/config.cpp
+++ KeyCardSystemServer/config.cpp
@@ -1,12 +1,3 @@
-////////////////////////////////////////////////////////////////////
-//DeRap: KeyCardSystemServer\config.bin
-//Produced from mikero's Dos Tools Dll version 7.97
-//https://mikero.bytex.digital/Downloads
-//'now' is Sat Sep 25 15:07:02 2021 : 'file' last modified on Sun Sep 12 15:29:04 2021
-////////////////////////////////////////////////////////////////////
-
-#define _ARMA_
-
 class CfgPatches
 {
 	class KeyCardSystemServer
--- KeyCardSystemServerConfig/config.cpp
+++ KeyCardSystemServerConfig/config.cpp
@@ -1,12 +1,3 @@
-////////////////////////////////////////////////////////////////////
-//DeRap: KeyCardSystemServerConfig\config.bin
-//Produced from mikero's Dos Tools Dll version 7.97
-//https://mikero.bytex.digital/Downloads
-//'now' is Sat Sep 25 15:07:04 2021 : 'file' last modified on Wed Sep 15 02:39:59 2021
-////////////////////////////////////////////////////////////////////
-
-#define _ARMA_
-
 class CfgPatches
 {
 	class KeyCardSystemServerConfig
