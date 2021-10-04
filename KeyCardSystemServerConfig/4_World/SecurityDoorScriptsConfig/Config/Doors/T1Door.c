ce57b848c42d07a6196ca360e3b3cf287707cd94
Manu Shamil <42031273+ManuShamil@users.noreply.github.com>
2021-10-04 06:41:18 +0530

    update

--- KeyCardSystemServerConfig/4_World/SecurityDoorScriptsConfig/Config/Doors/T1Door.c
+++ KeyCardSystemServerConfig/4_World/SecurityDoorScriptsConfig/Config/Doors/T1Door.c
@@ -9,8 +9,21 @@ modded class Land_KlimaX_T1Door
     }
     override void AddLoot( EntityAI crate )
     {
-        super.AddLoot( crate );
 
-        crate.GetInventory().CreateInInventory("AK74");
+        int rndIndex;
+		EntityAI weapon;
+		EntityAI optic;
+
+        rndIndex = Math.RandomInt(0,3);
+
+
+        weapon = crate.GetInventory().CreateInInventory("Winchester70_Black");
+        for (int i=0; i<3; i++) crate.GetInventory().CreateInInventory("Ammo_308WinTracer");
+        if ( weapon )
+        {
+            weapon.GetInventory().CreateAttachment("HuntingOptic");
+            weapon.GetInventory().CreateAttachment("GhillieAtt_Woodland");
+        }
+
     }
 };
--- KeyCardSystemServerConfig/4_World/SecurityDoorScriptsConfig/Config/Doors/T3Door.c
+++ KeyCardSystemServerConfig/4_World/SecurityDoorScriptsConfig/Config/Doors/T3Door.c
@@ -9,6 +9,19 @@ modded class Land_KlimaX_T3Door
 
     override void AddLoot( EntityAI crate )
     {
-        crate.GetInventory().CreateInInventory("M4A1");
+        int rndIndex;
+		EntityAI weapon;
+		EntityAI optic;
+
+        rndIndex = Math.RandomInt(0,3);
+
+
+        weapon = crate.GetInventory().CreateInInventory("Winchester70_Black");
+        for (int i=0; i<3; i++) crate.GetInventory().CreateInInventory("Ammo_308WinTracer");
+        if ( weapon )
+        {
+            weapon.GetInventory().CreateAttachment("HuntingOptic");
+            weapon.GetInventory().CreateAttachment("GhillieAtt_Woodland");
+        }
     }
 };
