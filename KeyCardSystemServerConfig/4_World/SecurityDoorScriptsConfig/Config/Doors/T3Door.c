f21be4588a206605307bc9e71988b5ad3f8a51cd
Manu Shamil <42031273+ManuShamil@users.noreply.github.com>
2021-10-05 00:48:58 +0530

    update

--- KeyCardSystemServerConfig/4_World/SecurityDoorScriptsConfig/Config/Doors/T3Door.c
+++ KeyCardSystemServerConfig/4_World/SecurityDoorScriptsConfig/Config/Doors/T3Door.c
@@ -9,19 +9,34 @@ modded class Land_KlimaX_T3Door
 
     override void AddLoot( EntityAI crate )
     {
+       int i;
+
         int rndIndex;
 		EntityAI weapon;
 		EntityAI optic;
 
-        rndIndex = Math.RandomInt(0,3);
-
 
         weapon = crate.GetInventory().CreateInInventory("Winchester70_Black");
-        for (int i=0; i<3; i++) crate.GetInventory().CreateInInventory("Ammo_308WinTracer");
         if ( weapon )
         {
             weapon.GetInventory().CreateAttachment("HuntingOptic");
             weapon.GetInventory().CreateAttachment("GhillieAtt_Woodland");
+        }
+
+        for (i=0; i<Math.RandomInt(1,4); i++) crate.GetInventory().CreateInInventory("Ammo_308WinTracer");
+
+        rndIndex = Math.RandomInt(0,2);
+        switch( rndIndex )
+        {
+            case 0:
+                weapon = crate.GetInventory().CreateInInventory("M4A1");
+                if ( weapon )
+                {
+                    for (i=0; i<Math.RandomInt(1,4); i++) crate.GetInventory().CreateInInventory("Mag_STANAG_30Rnd");
+                }
+                break;
+            case 1:
+                break;
         }
     }
 };
